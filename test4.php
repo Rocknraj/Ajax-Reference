11ef9e25f5b5fa820bc23f4b87a9afccd5bd0d4d
Rodger Kurth <rkurth@bluechipww.com>
2012-01-03 11:17:00 -0600

    Add to repository

---
+++ test4.php
@@ -0,0 +1,13 @@
+<?php
+
+//header("Content-type: application/JavaScript");
+
+$name=$_POST['name'];
+$isLong=(name.length()>8);
+updateName($name,$isLong);
+
+//echo "alert('".$_POST['inputName']."');";
+
+
+
+?>
